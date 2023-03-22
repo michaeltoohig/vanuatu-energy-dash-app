2ea5792ef8790bc6d6fe64e511d413aee119bd0a
Michael Toohig <michael.toohig@gmail.com>
2023-03-23 09:49:08 +1100

    Add 2023-02 tariff report

--- project/scripts/fetchUnelcoTariffReport.py
+++ project/scripts/fetchUnelcoTariffReport.py
@@ -2,12 +2,13 @@
 Fetch Unelco's latest electricity tariff report.
 Unelco will also provide via email upon request (not any longer).
 """
-# TODO
-
-from datetime import datetime
+from pathlib import Path
 import requests
 
-# URL of current month electricity tariff rate
 URL = "https://www.unelco.engie.com/images/doc/electricityrate.pdf"
-
 resp = requests.get(URL)
+resp.raise_for_status()
+output = Path("data/unelco-tariff-reports/downloaded.pdf").write_bytes(resp.content)
+
+# User must check file is new and give YYYY-MM.pdf filename.
+print("Check downloaded.pdf and rename YYYY-MM.pdf")
