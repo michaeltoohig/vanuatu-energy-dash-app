e02176e602ecef33edd46d6c6c1a107aeea85acb
Michael Toohig <michael.toohig@gmail.com>
2023-06-04 13:04:59 +1100

    Add check if output exists

--- project/scripts/fetchUnelcoTariffReport.py
+++ project/scripts/fetchUnelcoTariffReport.py
@@ -5,10 +5,14 @@ Unelco will also provide via email upon request (not any longer).
 from pathlib import Path
 import requests
 
+output = Path("data/unelco-tariff-reports/downloaded.pdf")
+if output.exists():
+    raise RuntimeError("Output file currently exists.")
+
 URL = "https://www.unelco.engie.com/images/doc/electricityrate.pdf"
 resp = requests.get(URL)
 resp.raise_for_status()
-output = Path("data/unelco-tariff-reports/downloaded.pdf").write_bytes(resp.content)
+output.write_bytes(resp.content)
 
 # User must check file is new and give YYYY-MM.pdf filename.
 print("Check downloaded.pdf and rename YYYY-MM.pdf")
